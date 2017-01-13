a0869d3fc66791031edba27371891d6742332518
Denys Voronin <dvoronin@oroinc.com>
2017-01-13 20:00:52 +0200

    CRM-7433: CRM and Commerce sample data (#6529)
    - do not install Magento sample data for CRM and Commerce integration

---
+++ src/Oro/Bundle/DemoDataCommerceCRMBundle/Command/LoadDataFixturesCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Oro\Bundle\DemoDataCommerceCRMBundle\Command;
+
+use Oro\Bundle\MigrationBundle\Command\LoadDataFixturesCommand as BaseDataFixturesCommand;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Override "oro:migration:data:load" command and add exclude sample data for Magento Bundle because it's not possible
+ * to modify arguments of the command through command listener.
+ *
+ * @todo: Should be moved to console listener after: https://github.com/symfony/symfony/issues/19441
+ */
+class LoadDataFixturesCommand extends BaseDataFixturesCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getOption('fixtures-type') === self::DEMO_FIXTURES_TYPE) {
+            $exclude = $input->getOption('exclude') ?: [];
+
+            $exclude = array_merge(
+                $exclude,
+                // exclude Demo data loading for Magento and related Bundles
+                ['OroMagentoBundle', 'OroAbandonedCartBundle', 'OroMarketingCRMBridgeBundle']
+            );
+            $input->setOption('exclude', $exclude);
+        }
+
+        return parent::execute($input, $output);
+    }
+}
---
+++ src/Oro/Bundle/DemoDataCommerceCRMBundle/OroDemoDataCommerceCRMBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Oro\Bundle\DemoDataCommerceCRMBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroDemoDataCommerceCRMBundle extends Bundle
+{
+}
