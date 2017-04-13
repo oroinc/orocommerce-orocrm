3e6447c1b58b80829557349821057301f1e549fd
Vladimir Seniuk <vseniuk@oroinc.com>
2017-04-13 11:28:20 +0300

    BB-8833: Create assotiation from ContactRequest to Customer - added migration (create ManyToOne relation contact request to customer user)

---
+++ src/Oro/Bridge/ContactUs/Migrations/Schema/OroContactUsBridgeBundleInstaller.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Oro\Bridge\ContactUs\Migrations\Schema;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\MigrationBundle\Migration\Installation;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
+
+class OroContactUsBridgeBundleInstaller implements
+    Installation,
+    ExtendExtensionAwareInterface
+{
+    /** @var ExtendExtension */
+    protected $extendExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setExtendExtension(ExtendExtension $extendExtension)
+    {
+        $this->extendExtension = $extendExtension;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMigrationVersion()
+    {
+        return 'v1_0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->extendExtension->addManyToOneRelation(
+            $schema,
+            'orocrm_contactus_request',
+            'customer_user',
+            'oro_customer_user',
+            'id',
+            [
+                'extend' => [
+                    'owner' => ExtendScope::OWNER_SYSTEM,
+                    'is_extend' => false,
+                ]
+            ]
+        );
+    }
+}
