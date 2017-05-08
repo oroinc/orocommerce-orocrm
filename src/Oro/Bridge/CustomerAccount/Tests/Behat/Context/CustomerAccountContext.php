1cdd23741b496ba644024512a7cd0de7f9a78d72
Daniel Nahrebecki <dnahrebecki@oroinc.com>
2017-05-08 11:55:42 +0200

    BB-9325: Postponed rows should be delayed - increase time for MQ processing in behat

--- src/Oro/Bridge/CustomerAccount/Tests/Behat/Context/CustomerAccountContext.php
+++ src/Oro/Bridge/CustomerAccount/Tests/Behat/Context/CustomerAccountContext.php
@@ -2,10 +2,34 @@
 
 namespace Oro\Bridge\CustomerAccount\Tests\Behat\Context;
 
-use Oro\Bundle\TestFrameworkBundle\Behat\Context\OroFeatureContext;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 
-class CustomerAccountContext extends OroFeatureContext
+use Oro\Bundle\ImportExportBundle\Tests\Behat\Context\ImportExportContext;
+use Oro\Bundle\TestFrameworkBundle\Behat\Context\OroFeatureContext;
+use Oro\Bundle\TestFrameworkBundle\Behat\Isolation\MessageQueueIsolatorAwareInterface;
+use Oro\Bundle\TestFrameworkBundle\Behat\Isolation\MessageQueueIsolatorInterface;
+
+class CustomerAccountContext extends OroFeatureContext implements MessageQueueIsolatorAwareInterface
 {
+    /**
+     * @var MessageQueueIsolatorInterface
+     */
+    private $messageQueueIsolator;
+
+    /**
+     * @var ImportExportContext
+     */
+    private $importExportContext;
+
+    /**
+     * @BeforeScenario
+     */
+    public function gatherContexts(BeforeScenarioScope $scope)
+    {
+        $environment = $scope->getEnvironment();
+        $this->importExportContext = $environment->getContext(ImportExportContext::class);
+    }
+
     /**
      * @Given /^Email should contains the following "([^"]*)" text$/
      * @param string $text
@@ -13,5 +37,23 @@ class CustomerAccountContext extends OroFeatureContext
     public function emailShouldContainsTheFollowingText($text)
     {
         //todo: to be implemented in scope of CRM-7599. Consulted with Serhii Polishchuk
+    }
+
+    /**
+     * @When /^I import customers file$/
+     */
+    public function iImportCustomersFile()
+    {
+        $this->importExportContext->iImportFile();
+        // need to wait for so long because of postpone processing of customers rows. See PostponeRowsHandler consts
+        $this->messageQueueIsolator->waitWhileProcessingMessages(600);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setMessageQueueIsolator(MessageQueueIsolatorInterface $messageQueueIsolator)
+    {
+        $this->messageQueueIsolator = $messageQueueIsolator;
     }
 }
