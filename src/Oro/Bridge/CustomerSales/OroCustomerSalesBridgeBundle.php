e5a4ba104a8292293a34169f11b98006721870e0
Yurii Muratov <ymuratov@oroinc.com>
2017-07-11 00:17:32 +0300

    BB-10146: Customers are not correctly deleted (#11111)

--- src/Oro/Bridge/CustomerSales/OroCustomerSalesBridgeBundle.php
+++ src/Oro/Bridge/CustomerSales/OroCustomerSalesBridgeBundle.php
@@ -2,8 +2,20 @@
 
 namespace Oro\Bridge\CustomerSales;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+use Oro\Bridge\CustomerSales\DependencyInjection\Compiler\CustomerAssignHelperPass;
 
 class OroCustomerSalesBridgeBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new CustomerAssignHelperPass());
+    }
 }
