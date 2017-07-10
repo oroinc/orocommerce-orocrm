bf801519d8578d2063e97f3658ea51257145550a
Yurii Muratov <ymuratov@oroinc.com>
2017-07-11 00:17:48 +0300

    BB-10146: Customers are not correctly deleted (#11166)

---
+++ src/Oro/Bridge/CustomerSales/DependencyInjection/Compiler/CustomerAssignHelperPass.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oro\Bridge\CustomerSales\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Oro\Bundle\CustomerBundle\Entity\Customer;
+use Oro\Bundle\SalesBundle\Entity\Customer as SalesCustomer;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+use Oro\Bundle\SalesBundle\EntityConfig\CustomerScope;
+
+class CustomerAssignHelperPass implements CompilerPassInterface
+{
+    const HELPER_SERVICE_NAME = 'oro_customer.customer.assign.helper';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition(self::HELPER_SERVICE_NAME)) {
+            return;
+        }
+
+        $associationName = ExtendHelper::buildAssociationName(
+            Customer::class,
+            CustomerScope::ASSOCIATION_KIND
+        );
+
+        $container->getDefinition(self::HELPER_SERVICE_NAME)
+            ->addMethodCall('addIgnoredRelation', [SalesCustomer::class, $associationName]);
+    }
+}
