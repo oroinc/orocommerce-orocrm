005cd04043ecdd8c0beb4d252647fbfc2ce8fbc7
Vitaliy Berdylo <vberdylo@oroinc.com>
2018-05-16 15:53:24 +0300

    BAP-17064: Remove Auto-registration of the command deprecation

--- src/Oro/Bundle/DemoDataCommerceCRMBundle/DependencyInjection/DemoDataCommerceCRMExtension.php
+++ src/Oro/Bundle/DemoDataCommerceCRMBundle/DependencyInjection/DemoDataCommerceCRMExtension.php
@@ -1,32 +1,23 @@
 <?php
 
-namespace Oro\Bridge\CustomerAccount\DependencyInjection;
+namespace Oro\Bundle\DemoDataCommerceCRMBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
-class OroCustomerAccountBridgeExtension extends Extension
+class DemoDataCommerceCRMExtension extends Extension
 {
+    const ALIAS = 'demo_data_commerce_crm';
+
     /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
         $loader->load('commands.yml');
-
-        $container->prependExtensionConfig($this->getAlias(), $config);
     }
 
     /**
@@ -34,6 +25,6 @@ class OroCustomerAccountBridgeExtension extends Extension
      */
     public function getAlias()
     {
-        return 'oro_customer_account_bridge';
+        return self::ALIAS;
     }
 }
