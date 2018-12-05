eba4ff447a721671ef7db392b0869de316a03458
Eugene Chetverikov <EugeneC@users.noreply.github.com>
2018-12-05 16:17:08 +0200

    BB-15804: Move logic of ContactRequest to commerce-crm package ContactUs bridge (#21162)
    - moved Contact Request logic to ContactUs bundle from ConsentBundle
    - system website configuration has been moved to MultiWebsiteBundle

--- src/Oro/Bridge/ContactUs/DependencyInjection/Configuration.php
+++ src/Oro/Bridge/ContactUs/DependencyInjection/Configuration.php
@@ -2,14 +2,16 @@
 
 namespace Oro\Bridge\ContactUs\DependencyInjection;
 
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
 use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    const ROOT_NODE = 'oro_contact_us_bridge';
+    const ROOT_NODE = OroContactUsBridgeExtension::ALIAS;
     const ENABLE_CONTACT_REQUEST = 'enable_contact_request';
+    const CONSENT_CONTACT_REASON = 'consent_contact_reason';
 
     /**
      * {@inheritDoc}
@@ -24,9 +26,21 @@ class Configuration implements ConfigurationInterface
             $rootNode,
             [
                 self::ENABLE_CONTACT_REQUEST => ['type' => 'boolean', 'value' => true],
+                self::CONSENT_CONTACT_REASON => ['value' => null, 'type' => 'integer'],
             ]
         );
 
         return $treeBuilder;
+    }
+
+
+    /**
+     * @param string $key
+     * @param string $separator
+     * @return string
+     */
+    public static function getConfigKey($key, $separator = ConfigManager::SECTION_MODEL_SEPARATOR)
+    {
+        return sprintf('%s%s%s', self::ROOT_NODE, $separator, $key);
     }
 }
