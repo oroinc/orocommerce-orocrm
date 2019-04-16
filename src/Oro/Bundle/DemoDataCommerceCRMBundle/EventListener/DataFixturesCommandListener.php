29f6cd7d6d23fb60824f74448cff341692aa231a
Mykhailo Sulyma <msulima@oroinc.com>
2019-04-16 13:22:15 +0300

    CRM-9131: Delete package/mailchimp from the monolithic repository (#23501)
     - updated codeowners file, jenkins script, documents log file and demo migration
     - removed LoadDataFixturesCommand from OroDemoDataCommerceCRMBundle, added DataFixturesCommandListener instead

---
+++ src/Oro/Bundle/DemoDataCommerceCRMBundle/EventListener/DataFixturesCommandListener.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Oro\Bundle\DemoDataCommerceCRMBundle\EventListener;
+
+use Oro\Bundle\MigrationBundle\Command\LoadDataFixturesCommand;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+
+/**
+ * Override the default value of the option "exclude" for the "oro:migration:data:load" command
+ * to exclude sample data from Magento Bundle.
+ */
+class DataFixturesCommandListener
+{
+    /**
+     * @param ConsoleCommandEvent $event
+     */
+    public function onConsoleCommand(ConsoleCommandEvent $event): void
+    {
+        $command = $event->getCommand();
+        if (!$command || $command->getName() !== LoadDataFixturesCommand::COMMAND_NAME) {
+            return;
+        }
+
+        $input = $event->getInput();
+        if ($input->getOption('fixtures-type') !== LoadDataFixturesCommand::DEMO_FIXTURES_TYPE) {
+            return;
+        }
+
+        $definition = $command->getDefinition();
+        if (!$definition->hasOption('exclude')) {
+            return;
+        }
+
+        // exclude Demo data loading for Magento and related Bundles
+        $option = $definition->getOption('exclude');
+        $option->setDefault(
+            array_merge(
+                (array) $option->getDefault(),
+                ['OroMagentoBundle', 'OroMarketingCRMBridgeBundle']
+            )
+        );
+    }
+}
---
+++ src/Oro/Bundle/DemoDataCommerceCRMBundle/Tests/Unit/EventListener/DataFixturesCommandListenerTest.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Oro\Bundle\DemoDataCommerceCRMBundle\Tests\Unit\EventListener;
+
+use Oro\Bundle\DemoDataCommerceCRMBundle\EventListener\DataFixturesCommandListener;
+use Oro\Bundle\MigrationBundle\Command\LoadDataFixturesCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+class DataFixturesCommandListenerTest extends \PHPUnit\Framework\TestCase
+{
+    /** @var DataFixturesCommandListener */
+    private $listener;
+
+    protected function setUp()
+    {
+        $this->listener = new DataFixturesCommandListener();
+    }
+
+    public function testOnConsoleCommand(): void
+    {
+        $option = $this->createMock(InputOption::class);
+        $option->expects($this->once())
+            ->method('getDefault')
+            ->willReturn(['OroTestBundle']);
+        $option->expects($this->once())
+            ->method('setDefault')
+            ->willReturn(['OroTestBundle', 'OroMagentoBundle', 'OroMarketingCRMBridgeBundle']);
+
+        $definition = $this->createMock(InputDefinition::class);
+        $definition->expects($this->once())
+            ->method('hasOption')
+            ->with('exclude')
+            ->willReturn(true);
+        $definition->expects($this->once())
+            ->method('getOption')
+            ->with('exclude')
+            ->willReturn($option);
+
+        $command = $this->createMock(Command::class);
+        $command->expects($this->once())
+            ->method('getName')
+            ->willReturn(LoadDataFixturesCommand::COMMAND_NAME);
+        $command->expects($this->once())
+            ->method('getDefinition')
+            ->willReturn($definition);
+
+        $input = $this->createMock(InputInterface::class);
+        $input->expects($this->once())
+            ->method('getOption')
+            ->with('fixtures-type')
+            ->willReturn(LoadDataFixturesCommand::DEMO_FIXTURES_TYPE);
+
+        $this->listener->onConsoleCommand($this->getEvent($command, $input));
+    }
+
+    public function testOnConsoleCommandNoCommand(): void
+    {
+        $this->listener->onConsoleCommand($this->getEvent());
+    }
+
+    public function testOnConsoleCommandIncorrectCommandName(): void
+    {
+        $command = $this->createMock(Command::class);
+        $command->expects($this->once())
+            ->method('getName')
+            ->willReturn('test_name');
+        $command->expects($this->never())
+            ->method('getDefinition');
+
+        $this->listener->onConsoleCommand($this->getEvent($command));
+    }
+
+    public function testOnConsoleCommandIncorrectFixturesType(): void
+    {
+        $command = $this->createMock(Command::class);
+        $command->expects($this->once())
+            ->method('getName')
+            ->willReturn(LoadDataFixturesCommand::COMMAND_NAME);
+        $command->expects($this->never())
+            ->method('getDefinition');
+
+        $input = $this->createMock(InputInterface::class);
+        $input->expects($this->once())
+            ->method('getOption')
+            ->with('fixtures-type')
+            ->willReturn('test_type');
+
+        $this->listener->onConsoleCommand($this->getEvent($command, $input));
+    }
+
+    public function testOnConsoleCommandNoExcludeOption(): void
+    {
+        $definition = $this->createMock(InputDefinition::class);
+        $definition->expects($this->once())
+            ->method('hasOption')
+            ->with('exclude')
+            ->willReturn(false);
+        $definition->expects($this->never())
+            ->method('getOption');
+
+        $command = $this->createMock(Command::class);
+        $command->expects($this->once())
+            ->method('getName')
+            ->willReturn(LoadDataFixturesCommand::COMMAND_NAME);
+        $command->expects($this->once())
+            ->method('getDefinition')
+            ->willReturn($definition);
+
+        $input = $this->createMock(InputInterface::class);
+        $input->expects($this->once())
+            ->method('getOption')
+            ->with('fixtures-type')
+            ->willReturn(LoadDataFixturesCommand::DEMO_FIXTURES_TYPE);
+
+        $this->listener->onConsoleCommand($this->getEvent($command, $input));
+    }
+
+    /**
+     * @param \PHPUnit\Framework\MockObject\MockObject|Command|null $command
+     * @param \PHPUnit\Framework\MockObject\MockObject|InputInterface|null $input
+     * @return \PHPUnit\Framework\MockObject\MockObject|ConsoleCommandEvent
+     */
+    private function getEvent(Command $command = null, InputInterface $input = null): ConsoleCommandEvent
+    {
+        /** @var ConsoleCommandEvent|\PHPUnit\Framework\MockObject\MockObject $event */
+        $event = $this->createMock(ConsoleCommandEvent::class);
+        $event->expects($this->once())
+            ->method('getCommand')
+            ->willReturn($command);
+        $event->expects($input ? $this->once() : $this->never())
+            ->method('getInput')
+            ->willReturn($input);
+
+        return $event;
+    }
+}
