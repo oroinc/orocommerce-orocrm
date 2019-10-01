e4502c7b63d52ed78047bcdbcf7523b598a63823
Viacheslav Bemza <vbemza@oroinc.com>
2019-10-01 17:48:07 +0300

    BB-17341: Customer Hierarchy Navigation (#24955)
    - added parent chain and subsidiaries grid
    - covered by unit and behat tests
    - increase total queries limit for customer view page because of add new grid + account data information
    - created collapse-view styles for Management Console

---
+++ src/Oro/Bridge/CustomerAccount/EventListener/AccountCustomerViewListener.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Oro\Bridge\CustomerAccount\EventListener;
+
+use Oro\Bundle\CustomerBundle\Entity\Customer;
+use Oro\Bundle\SalesBundle\Entity\Manager\AccountCustomerManager;
+use Oro\Bundle\UIBundle\Event\BeforeListRenderEvent;
+
+/**
+ * Listener adds account information to commerce customer page.
+ */
+class AccountCustomerViewListener
+{
+    /**
+     * @var AccountCustomerManager
+     */
+    protected $accountCustomerManager;
+
+    /**
+     * @param AccountCustomerManager $accountCustomerManager
+     */
+    public function __construct(AccountCustomerManager $accountCustomerManager)
+    {
+        $this->accountCustomerManager = $accountCustomerManager;
+    }
+
+    /**
+     * @param BeforeListRenderEvent $event
+     */
+    public function onView(BeforeListRenderEvent $event)
+    {
+        /** @var Customer $customer */
+        $customer = $event->getEntity();
+        if (!$customer instanceof Customer) {
+            return;
+        }
+
+        $salesCustomer = $this->accountCustomerManager->getAccountCustomerByTarget($customer, false);
+        $template = $event->getEnvironment()->render(
+            'OroCustomerAccountBridgeBundle:Customer:accountView.html.twig',
+            ['salesCustomer' => $salesCustomer]
+        );
+        $event->getScrollData()->addSubBlockData(0, 0, $template);
+    }
+}
