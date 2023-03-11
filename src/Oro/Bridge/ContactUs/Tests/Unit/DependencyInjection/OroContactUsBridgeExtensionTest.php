6cbb8a3eff35f2f24410be801ade5ecc00bb2b5e
Vova Soroka <vsoroka@oroinc.com>
2023-03-11 17:06:47 +0200

    BAP-21930: Refactor DI extensions for bundles (#35041)
    - add getSettings() method to Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder that simplifies loading of system settings into DIC in DI extension
    - refactor loading of system settings into DIC in DI extensions
    - add strict type to DI extensions
    - refactor unit tests for DI extensions

--- src/Oro/Bridge/ContactUs/Tests/Unit/DependencyInjection/OroContactUsBridgeExtensionTest.php
+++ src/Oro/Bridge/ContactUs/Tests/Unit/DependencyInjection/OroContactUsBridgeExtensionTest.php
@@ -3,17 +3,29 @@
 namespace Oro\Bridge\ContactUs\Tests\Unit\DependencyInjection;
 
 use Oro\Bridge\ContactUs\DependencyInjection\OroContactUsBridgeExtension;
-use Oro\Bundle\TestFrameworkBundle\Test\DependencyInjection\ExtensionTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class OroContactUsBridgeExtensionTest extends ExtensionTestCase
+class OroContactUsBridgeExtensionTest extends \PHPUnit\Framework\TestCase
 {
-    public function testLoad()
+    public function testLoad(): void
     {
-        $this->loadExtension(new OroContactUsBridgeExtension());
+        $container = new ContainerBuilder();
 
-        $expectedServices = [
-            'oro_contact_us_bridge.contact_request_type',
-        ];
-        $this->assertDefinitionsLoaded($expectedServices);
+        $extension = new OroContactUsBridgeExtension();
+        $extension->load([], $container);
+
+        self::assertNotEmpty($container->getDefinitions());
+        self::assertSame(
+            [
+                [
+                    'settings' => [
+                        'resolved' => true,
+                        'enable_contact_request' => ['value' => true, 'scope' => 'app'],
+                        'consent_contact_reason' => ['value' => null, 'scope' => 'app'],
+                    ]
+                ]
+            ],
+            $container->getExtensionConfig('oro_contact_us_bridge')
+        );
     }
 }
