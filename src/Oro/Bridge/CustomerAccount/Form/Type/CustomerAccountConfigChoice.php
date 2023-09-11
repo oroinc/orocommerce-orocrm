bf85ef8dbc24659eb468cbdabe40d1828e2c92ee
Andrii Yatsenko <ayatsenko@oroinc.com>
2023-09-11 21:55:52 +0300

    BAP-22238: Use Symfony 6 return types (#36389)

--- src/Oro/Bridge/CustomerAccount/Form/Type/CustomerAccountConfigChoice.php
+++ src/Oro/Bridge/CustomerAccount/Form/Type/CustomerAccountConfigChoice.php
@@ -12,7 +12,7 @@ class CustomerAccountConfigChoice extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
+    public function getBlockPrefix(): string
     {
         return self::NAME;
     }
@@ -20,7 +20,7 @@ class CustomerAccountConfigChoice extends AbstractType
     /**
      * {@inheritDoc}
      */
-    public function getParent()
+    public function getParent(): ?string
     {
         return ChoiceType::class;
     }
