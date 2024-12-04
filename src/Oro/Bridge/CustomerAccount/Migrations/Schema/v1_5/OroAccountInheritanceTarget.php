2d66b0ef532df806d587d6d7c8fb94392f483a1d
yevhenii101991 <146938608+yevhenii101991@users.noreply.github.com>
2024-12-04 15:06:36 +0200

    BB-24879: OroAccountInheritanceTarget schema migration fails when upgrading from release/5.1 to release/6.0 tag (#39897)
    * BB-24879: OroAccountInheritanceTarget schema migration fails when upgrading from release/5.1 to release/6.0 tag
    
    * BB-24879: added unit test
    
    ---------
    
    Co-authored-by: Yevhenii Zhadan <yzhadan@oroinc.com>

--- src/Oro/Bridge/CustomerAccount/Migrations/Schema/v1_5/OroAccountInheritanceTarget.php
+++ src/Oro/Bridge/CustomerAccount/Migrations/Schema/v1_5/OroAccountInheritanceTarget.php
@@ -3,11 +3,14 @@
 namespace Oro\Bridge\CustomerAccount\Migrations\Schema\v1_5;
 
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Types;
 use Oro\Bundle\AccountBundle\Entity\Account;
 use Oro\Bundle\ActivityListBundle\Helper\ActivityInheritanceTargetsHelper;
 use Oro\Bundle\EntityConfigBundle\Migration\UpdateEntityConfigEntityValueQuery;
 use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
 use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareTrait;
+use Oro\Bundle\MigrationBundle\Migration\ConnectionAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\ConnectionAwareTrait;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 use Oro\Bundle\SalesBundle\Entity\Manager\AccountCustomerManager;
@@ -17,10 +20,15 @@ use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 /**
  * Update oro account inheritance target.
  */
-class OroAccountInheritanceTarget implements Migration, ContainerAwareInterface, ExtendExtensionAwareInterface
+class OroAccountInheritanceTarget implements
+    Migration,
+    ContainerAwareInterface,
+    ExtendExtensionAwareInterface,
+    ConnectionAwareInterface
 {
     use ContainerAwareTrait;
     use ExtendExtensionAwareTrait;
+    use ConnectionAwareTrait;
 
     #[\Override]
     public function up(Schema $schema, QueryBag $queries): void
@@ -52,6 +60,9 @@ class OroAccountInheritanceTarget implements Migration, ContainerAwareInterface,
         if (!$isUpdateNeeded) {
             return;
         }
+
+        $this->updateViewsOnPageParameter();
+
         $queries->addPreQuery(
             new UpdateEntityConfigEntityValueQuery(
                 Account::class,
@@ -65,5 +76,53 @@ class OroAccountInheritanceTarget implements Migration, ContainerAwareInterface,
     protected function getInheritanceTargetHelper(): ?ActivityInheritanceTargetsHelper
     {
         return $this->container->get('oro_activity_list.helper.activity_inheritance_targets');
+    }
+
+    private function updateViewsOnPageParameter(): void
+    {
+        $configData = $this->loadConfigData();
+        $this->processConfigData($configData);
+    }
+
+    private function loadConfigData(): array
+    {
+        $sql = sprintf(
+            'SELECT %s, %s FROM %s',
+            'id',
+            'data',
+            'oro_entity_config'
+        );
+
+        $configValues = $this->connection->fetchAllAssociative($sql);
+
+        $configData = [];
+        foreach ($configValues as $configValue) {
+            $configData[$configValue['id']] = $configValue['data'];
+        }
+
+        return $configData;
+    }
+
+    private function processConfigData(array $configData): void
+    {
+        foreach ($configData as $id => $data) {
+            $data = $this->connection->convertToPHPValue($data, Types::ARRAY);
+            if (!empty($data['activity']['show_on_page']) && is_string($data['activity']['show_on_page'])) {
+                $data['activity']['show_on_page'] = constant($data['activity']['show_on_page']);
+                $this->saveConfigData($id, $data);
+            }
+        }
+    }
+
+    private function saveConfigData(int $id, array $data): void
+    {
+        $sql = sprintf('UPDATE %s SET data = :data WHERE id = :id', 'oro_entity_config');
+
+        $parameters = [
+            'data' => $data,
+            'id' => $id,
+        ];
+        $types = ['id' => Types::INTEGER, 'data' => Types::ARRAY];
+        $this->connection->executeStatement($sql, $parameters, $types);
     }
 }
