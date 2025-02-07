15d0bfe83a6ea495a557ad272cefa023fcb4a984
Yurii Muratov <ymuratov@oroinc.com>
2025-02-07 17:26:36 +0200

    BAP-20198: Remove WSSE authentication (#40279)

--- src/Oro/Bridge/QuoteSales/Tests/Functional/Workflow/QuoteWorkflowCreateTest.php
+++ src/Oro/Bridge/QuoteSales/Tests/Functional/Workflow/QuoteWorkflowCreateTest.php
@@ -18,7 +18,7 @@ class QuoteWorkflowCreateTest extends WebTestCase
     #[\Override]
     protected function setUp(): void
     {
-        $this->initClient([], $this->generateWsseAuthHeader());
+        $this->initClient([], self::generateApiAuthHeader());
         $this->client->useHashNavigation(true);
 
         $this->loadFixtures([
