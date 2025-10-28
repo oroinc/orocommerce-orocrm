c4a5c1d17729723aded2feb372ed78b67dc9d785
Alex Yegerev <1755578+lexxcode@users.noreply.github.com>
2025-10-28 22:36:44 +0400

    BB-26295: Convert crm, commerce-crm, commerce-enterprise, authorizenet, apruve, ai-smart-order (#42758)
    * BB-26295: Convert crm, commerce-crm, commerce-enterprise, authorizenet, apruve, ai-smart-order
    
    ---------
    
    Co-authored-by: Valerii Yustyniuk <vyustyniuk@oroinc.com>

--- src/Oro/Bridge/CustomerAccount/Resources/public/js/app/account-creating-strategy/view/use-default-view.js
+++ src/Oro/Bridge/CustomerAccount/Resources/public/js/app/account-creating-strategy/view/use-default-view.js
@@ -1,26 +1,22 @@
-define(function(require) {
-    'use strict';
+import $ from 'jquery';
+import BaseView from 'oroui/js/app/views/base/view';
+import mediator from 'oroui/js/mediator';
 
-    const $ = require('jquery');
-    const BaseView = require('oroui/js/app/views/base/view');
-    const mediator = require('oroui/js/mediator');
+const UseSefaultView = BaseView.extend({
+    events: {
+        'change input[type="checkbox"]': '_handleChangeUseDefault'
+    },
 
-    const UseSefaultView = BaseView.extend({
-        events: {
-            'change input[type="checkbox"]': '_handleChangeUseDefault'
-        },
+    /**
+     * @inheritdoc
+     */
+    constructor: function UseSefaultView(options) {
+        UseSefaultView.__super__.constructor.call(this, options);
+    },
 
-        /**
-         * @inheritdoc
-         */
-        constructor: function UseSefaultView(options) {
-            UseSefaultView.__super__.constructor.call(this, options);
-        },
-
-        _handleChangeUseDefault: function(e) {
-            mediator.trigger('strategy-creation-account:changeUseDefault', $(e.target).is(':checked'));
-        }
-    });
-
-    return UseSefaultView;
+    _handleChangeUseDefault: function(e) {
+        mediator.trigger('strategy-creation-account:changeUseDefault', $(e.target).is(':checked'));
+    }
 });
+
+export default UseSefaultView;
--- src/Oro/Bridge/CustomerAccount/Resources/public/js/app/account-creating-strategy/view/warning-view.js
+++ src/Oro/Bridge/CustomerAccount/Resources/public/js/app/account-creating-strategy/view/warning-view.js
@@ -1,87 +1,83 @@
-define(function(require) {
-    'use strict';
+import $ from 'jquery';
+import BaseView from 'oroui/js/app/views/base/view';
 
-    const $ = require('jquery');
-    const BaseView = require('oroui/js/app/views/base/view');
+const WarningSettingsView = BaseView.extend({
+    $messageContainer: null,
+    $selectStrategy: null,
+    rootMessage: null,
+    eachMessage: null,
+    currentStrategy: null,
+    defaultStrategy: null,
 
-    const WarningSettingsView = BaseView.extend({
-        $messageContainer: null,
-        $selectStrategy: null,
-        rootMessage: null,
-        eachMessage: null,
-        currentStrategy: null,
-        defaultStrategy: null,
+    events: {
+        'change select': '_handleChangeSelect'
+    },
 
-        events: {
-            'change select': '_handleChangeSelect'
-        },
+    listen: {
+        'strategy-creation-account:changeUseDefault mediator': '_handleChangeUseDefault'
+    },
 
-        listen: {
-            'strategy-creation-account:changeUseDefault mediator': '_handleChangeUseDefault'
-        },
+    /**
+     * @inheritdoc
+     */
+    constructor: function WarningSettingsView(options) {
+        WarningSettingsView.__super__.constructor.call(this, options);
+    },
 
-        /**
-         * @inheritdoc
-         */
-        constructor: function WarningSettingsView(options) {
-            WarningSettingsView.__super__.constructor.call(this, options);
-        },
+    /**
+     * @inheritdoc
+     */
+    initialize: function(options) {
+        this.$messageContainer = this.$el.find(options.warningContainer);
+        this.$selectStrategy = this.$el.find('select');
 
-        /**
-         * @inheritdoc
-         */
-        initialize: function(options) {
-            this.$messageContainer = this.$el.find(options.warningContainer);
-            this.$selectStrategy = this.$el.find('select');
+        this.rootMessage = options.rootMessage;
+        this.eachMessage = options.eachMessage;
+        this.defaultStrategy = options.defaultStrategy;
+        this.currentStrategy = this._getSelectedStrategy();
 
-            this.rootMessage = options.rootMessage;
-            this.eachMessage = options.eachMessage;
-            this.defaultStrategy = options.defaultStrategy;
-            this.currentStrategy = this._getSelectedStrategy();
+        this._hideMessages();
+    },
 
-            this._hideMessages();
-        },
+    _handleChangeSelect: function(e) {
+        const strategy = $(e.target).val();
+        this._changeStrategy(strategy);
+    },
 
-        _handleChangeSelect: function(e) {
-            const strategy = $(e.target).val();
-            this._changeStrategy(strategy);
-        },
+    _handleChangeUseDefault: function(isDefault) {
+        if (isDefault === true) {
+            this.$selectStrategy.val(this.defaultStrategy).trigger('change');
 
-        _handleChangeUseDefault: function(isDefault) {
-            if (isDefault === true) {
-                this.$selectStrategy.val(this.defaultStrategy).trigger('change');
-
-                this._changeStrategy(this.defaultStrategy);
-            } else {
-                this._changeStrategy(this._getSelectedStrategy());
-            }
-        },
-
-        _changeStrategy: function(strategy) {
-            this._hideMessages();
-            if (strategy !== this.currentStrategy) {
-                this._showMessage(strategy);
-            }
-        },
-
-        _showMessage: function(strategy) {
-            if (strategy === 'root') {
-                this.$messageContainer.text(this.rootMessage);
-            } else {
-                this.$messageContainer.text(this.eachMessage);
-            }
-
-            this.$messageContainer.show();
-        },
-
-        _hideMessages: function() {
-            this.$messageContainer.hide();
-        },
-
-        _getSelectedStrategy: function() {
-            return this.$selectStrategy.val();
+            this._changeStrategy(this.defaultStrategy);
+        } else {
+            this._changeStrategy(this._getSelectedStrategy());
         }
-    });
+    },
 
-    return WarningSettingsView;
+    _changeStrategy: function(strategy) {
+        this._hideMessages();
+        if (strategy !== this.currentStrategy) {
+            this._showMessage(strategy);
+        }
+    },
+
+    _showMessage: function(strategy) {
+        if (strategy === 'root') {
+            this.$messageContainer.text(this.rootMessage);
+        } else {
+            this.$messageContainer.text(this.eachMessage);
+        }
+
+        this.$messageContainer.show();
+    },
+
+    _hideMessages: function() {
+        this.$messageContainer.hide();
+    },
+
+    _getSelectedStrategy: function() {
+        return this.$selectStrategy.val();
+    }
 });
+
+export default WarningSettingsView;
